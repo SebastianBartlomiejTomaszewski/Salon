d06d06e73f924ac00e1e0b6b9177d6a40e101d94
Sebastian Tomaszewski <sebastian.tomaszewski@nexteer.com>
2019-02-14 11:56:50 +0100

    widoki

---
+++ Salon/rapOp/Views/W_ModeleBezZamowien.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [rapOp].[W_ModeleBezZamowien] AS
+	SELECT [m].[Model_Nazwa]
+	FROM [dbo].[Model] AS m WITH (NOLOCK)	LEFT JOIN [dbo].[Zamowienia] AS z ON [m].[Model_Id]=[z].[Model_Id] 
+	WHERE [z].[Zamowienia_ID] IS NULL;
---
+++ Salon/rapOp/Views/W_PracownicyZamowienie.sql
@@ -0,0 +1,4 @@
+﻿CREATE VIEW [rapOp].[W_PracownicyZamowienie] AS
+	SELECT [p].[Pracownik_Nazwisko], p.[Pracownik_Imie], COUNT(z.Zamowienia_Id) AS [IloscZamowien], SUM(z.Zamowienia_Cena) AS [KwotaZamowienia]
+	FROM [dbo].[Pracownik] AS p WITH (NOLOCK) LEFT JOIN [dbo].[Zamowienia] AS z ON p.Pracownik_Id = z.Pracownik_Id
+	GROUP BY p.Pracownik_Id, [p].[Pracownik_Nazwisko], p.[Pracownik_Imie]
