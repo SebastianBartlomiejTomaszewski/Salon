a89e0274349b7e25d41f77dd3aea291d40865abc
Sebastian Tomaszewski <sebastian.tomaszewski@nexteer.com>
2019-02-14 02:59:52 +0100

    xq

--- Salon/dbo/Tables/Cennik.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Cennik] (
-    [Cennik_Id]     INT             IDENTITY (1, 1) NOT NULL,
-    [Cennik_Cena]   DECIMAL (12, 2) NOT NULL,
-    [Cennik_DataOd] DATE            NOT NULL,
-    [Cennik_DataDo] DATE            NULL,
-    PRIMARY KEY CLUSTERED ([Cennik_Id] ASC)
-);
-
--- Salon/dbo/Tables/Pracownik.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Pracownik] (
-    [Pracownik_Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [Pracownik_PESEL]    NCHAR (11)    NOT NULL,
-    [Pracownik_Nazwisko] NVARCHAR (50) NOT NULL,
-    [Pracownik_Imie]     NVARCHAR (50) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Pracownik_Id] ASC),
-    UNIQUE NONCLUSTERED ([Pracownik_PESEL] ASC)
-);
-
